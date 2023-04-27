5407d7b22c16228eff852a6a75e0bc98ff0d5f26
sandro520099 <sandro520099@hotmail.com>
2023-04-27 19:40:22 -0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,3 @@
-<meta charset="UTF-8">    
 <script>
         var terminal = document.getElementById("terminal");
         var text = "تم مسح الكمبيوتر ..............................................................................................\n";
